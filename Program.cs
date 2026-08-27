2f1f6438de5ff4d1c6fe1da7647fb4f98f0de5e6
Sueneé <info@easyweby.cz>
2026-08-28 01:07:16 +0200

    Bump version to 2.03

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace YouTubeSubs;
 
 internal static class Program
 {
-    public const string Version = "2.02";
+    public const string Version = "2.03";
     private const int GuiPort = 45871;
 
     [STAThread]
