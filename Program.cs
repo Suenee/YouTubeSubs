d4553141c6d726ba6b2671419272e181190485dd
Sueneé <info@easyweby.cz>
2026-09-04 01:51:21 +0200

    Bump GUI to 2.19

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace YouTubeSubs;
 
 internal static class Program
 {
-    public const string Version = "2.18";
+    public const string Version = "2.19";
     private const int GuiPort = 45871;
 
     [STAThread]
