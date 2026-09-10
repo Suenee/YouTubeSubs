cc7f7f0ef64dd9978c38cb3bc0683418d32372ab
Sueneé <info@easyweby.cz>
2026-09-10 23:33:13 +0200

    Bump GUI version to 2.24

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace YouTubeSubs;
 
 internal static class Program
 {
-    public const string Version = "2.23";
+    public const string Version = "2.24";
     private const int GuiPort = 45871;
 
     [STAThread]
